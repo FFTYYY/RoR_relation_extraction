6fd9090ffc6fe084ea8f0448894e3f707940825a
YYY <1004473299@qq.com>
2020-04-16 21:53:09 +0800

    整理文件

--- analyze_res/compare/_.py
+++
@@ -1,7 +0,0 @@
-import json
-from pprint import pprint
-
-with open("ace2005-wl.json" , "r") as f:
-	a = json.load(f)
-
-pprint (a[0])
--- prep_data_and_env.sh
+++ prep_data_and_env.sh
@@ -1,4 +1,3 @@
-cd ..
 mkdir -p data/semeval_2018_task7/
 curl -o data/semeval_2018_task7/2.test.text.xml https://lipn.univ-paris13.fr/~gabor/semeval2018task7/2.test.text.xml
 curl -o data/semeval_2018_task7/keys.test.2.txt https://lipn.univ-paris13.fr/~gabor/semeval2018task7/keys.test.2.txt
@@ -10,10 +9,5 @@ curl -o data/semeval_2018_task7/keys.test.1.1.txt https://lipn.univ-paris13.fr/~
 curl -o data/semeval_2018_task7/1.2.text.xml https://lipn.univ-paris13.fr/~gabor/semeval2018task7/1.2.text.xml
 curl -o data/semeval_2018_task7/1.2.relations.txt https://lipn.univ-paris13.fr/~gabor/semeval2018task7/1.2.relations.txt
 curl -o data/semeval_2018_task7/semeval2018_task7_scorer-v1.2.pl https://lipn.univ-paris13.fr/~gabor/semeval2018task7/semeval2018_task7_scorer-v1.2.pl
-
-python data/agenda/reader.py
-python data/webnlg_sent/reader.py
-python data/meta/json2xml.py
-python data/meta/json2xml.py -dataset agenda
 
 pip install -r requirements.txt
